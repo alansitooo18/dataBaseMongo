5fad09822d7597d4d6421615b8ad9348ff6be56e
alansitooo18 <168034988+alansitooo18@users.noreply.github.com>
2024-04-26 19:01:26 -0600

    Update prueba.cs
    se cambió la condición a 9 para que no tome el 10 como valor.

--- dataBase/prueba.cs
+++ dataBase/prueba.cs
@@ -43,7 +43,7 @@ namespace dataBase
             var salesCollection = ventasDB.GetCollection<Ventas>("sales");
 
             //Filtro para encontrar un objeto en la base de datos que su price sea menor a 10
-            var filter = Builders<Ventas>.Filter.Lt("price", 10);
+            var filter = Builders<Ventas>.Filter.Lt("price", 9);
             //Lista de los objetos encontrados en el find 
             List<Ventas> salesList = salesCollection.Find(d =>true).ToList(); 
             //Imprimir Lista
